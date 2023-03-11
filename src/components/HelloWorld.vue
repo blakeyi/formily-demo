008ef27c2b0d3ecb0b0cadd845eae75c14800348
blakeyi <1152420971@qq.com>
2023-03-11 11:47:28 +0800

    chore:update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <Form :form="form">
+    <SchemaField :schema="schema" />
+    <Submit @submit="onSubmit">提交</Submit>
+  </Form>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { createForm } from "@formily/core";
+import { createSchemaField } from "@formily/vue";
+import { Form, FormItem, Input, Submit } from "@formily/element";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const schema = {
+  type: "object",
+  properties: {
+    input: {
+      type: "string",
+      title: "输入框",
+      "x-decorator": "FormItem",
+      "x-component": "Input",
+    },
+    textarea: {
+      type: "string",
+      title: "文本框",
+      "x-decorator": "FormItem",
+      "x-component": "Input.TextArea",
+    },
+  },
+};
+
+const form = createForm();
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Input,
+  },
+});
+
+export default {
+  components: { Form, SchemaField, Submit },
+  data() {
+    return {
+      form,
+      schema,
+    };
+  },
+  methods: {
+    onSubmit(value) {
+      console.log(value);
+    },
+  },
+};
+</script>
